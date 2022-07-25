5476d789d8f1a6d15dc9c4c006e3f5e577d05e30
wang-hsoo <dhkd4873@naver.com>
2022-07-26 01:19:08 +0900

    user maxDivision

--- src/Routes/Components/UserDetail.tsx
+++ src/Routes/Components/UserDetail.tsx
@@ -1,14 +1,70 @@
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
-import { ImatchType, IsearchUser, IuserDivision, matchType, userMaxdivision } from "../../api";
+import styled from "styled-components";
+import { ImatchType, ImaxDivision, IsearchUser, IuserDivision, matchType, userMaxdivision } from "../../api";
+
+const MaxList = styled.div`
+    width: 80%;
+`
+
+const MaxContent = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 100%;
+    place-items: center;
+
+`
+const Row = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: space-around;
+`
 
 function UserDetail({accessId, nickname, level}:IsearchUser){
     const {data, isLoading} = useQuery<IuserDivision>(["maxDivision"], () => userMaxdivision(accessId));
     const {data:mathCode} = useQuery<ImatchType>(["matchType"], matchType);
-    console.log(data);    
+    const [maxDivision, setMaxDivision] = useState<any>();
+    const [division, setDivision] = useState<any>();
 
+    
+     useEffect(() => {
+        if(data){
+            const getMaxDivision = {
+                default: data
+            }
+
+            const divisionCheck = {
+                default: mathCode
+            }
+
+            setMaxDivision(getMaxDivision);
+            setDivision(divisionCheck);
+        }
+     },[data])
+
+     console.log(mathCode);
+     
     return(
         <div>
             <h1>{nickname}</h1>
+            <MaxList>
+                <Row>
+                    <div>매치종류</div>
+                    <div>디비전</div>
+                    <div>달성일자</div>
+                </Row>
+                {maxDivision && maxDivision?.default.map((info:any) => (
+                    <MaxContent>
+
+                        <div>{division && division?.default.map((match:any) => (
+                            <div>{match.matchtype === info.matchType ? match.desc : null}</div>
+                        ))}</div>
+                        <div>{info.division}</div>
+                        <div>{info.achievementDate}</div>
+                    </MaxContent>
+                ))}
+            </MaxList>
         </div>
     )
 }
--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,9 @@
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjEwOTExMzM5MTIiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY3NDIxODIwNiwiaWF0IjoxNjU4NjY2MjA2LCJuYmYiOjE2NTg2NjYyMDYsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.oXNyt6Rnm_SWZISdygedwRNkerFoU55xbAxDj-oB3AY';
 
 export interface IsearchUser{
-  "accessId": string,
-  "nickname": string,
-  "level": number
+  accessId: string,
+  nickname: string,
+  level: number
 }
 
 export async function searchUser(userName:string){
@@ -11,18 +11,27 @@ export async function searchUser(userName:string){
 }
 
 export interface IuserDivision{
-    "matchType": number,
-    "division": number,
-    "achievementDate": string
+    matchType: number,
+    division: number,
+    achievementDate: string
 }
 
+export interface ImaxDivision{
+  Promise:  IuserDivision[];
+  
+}
+
+
+
 export async function userMaxdivision(accessid:string){
-  return fetch(`https://api.nexon.co.kr/fifaonline4/v1.0/users/${accessid}/maxdivision`, {headers:{Authorization:  API_KEY}}).then((response => response.json()))
+  const a = fetch(`https://api.nexon.co.kr/fifaonline4/v1.0/users/${accessid}/maxdivision`, {headers:{Authorization:  API_KEY}}).then((response => response.json()))
+  
+  return a;
 }
 
 export interface ImatchType{
-  "matchtype": number,
-  "desc": string
+  matchtype: number,
+  desc: string
 }
 
 export async function matchType(){
